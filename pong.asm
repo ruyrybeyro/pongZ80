1bee4e7e4267fcca026c8509de09d26aa65b968e
Rui Ribeiro <ruyrybeyro@gmail.com>
2020-03-21 21:43:03 +0000

    pong for the 48K ZX Spectrum-

---
+++ pong.asm
@@ -0,0 +1,29 @@
+
+	ORG	50000
+	CALL	INIT_GAME
+	CALL	INIT_BALL
+LOOP:	HALT
+	HALT
+	CALL	READ_KEYS
+	CALL	BALL_MOVE
+	CALL	PRINT_CYCLE
+	CALL	COLISION_DETECTION
+	JR	LOOP
+;	CALL	CLOSE_GAME
+	RET
+
+	INCLUDE	equ.inc
+	INCLUDE	init.inc
+	INCLUDE	close.inc
+	INCLUDE	ball.inc
+	INCLUDE players.inc
+	INCLUDE	collision.inc
+	INCLUDE	score.inc
+	INCLUDE	print.inc
+	INCLUDE	keyboard.inc
+	INCLUDE	beeper.inc
+	INCLUDE	colour.inc
+	INCLUDE	vars.inc
+	INCLUDE	udg.inc
+
+	END	50000
